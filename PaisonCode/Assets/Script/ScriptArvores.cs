3da0bb8ab13a05c92214e002230b2c1ce9e6e8e9
Yuji <perobastyle@gmail.com>
2024-08-23 01:25:37 -0300

    ajeitei as arvores

---
+++ PaisonCode/Assets/Script/ScriptArvores.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptArvores : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        AdjustSortingOrder();
+    }
+
+    void AdjustSortingOrder()
+    {
+        // Ajusta a ordem de renderização com base na posição Y
+        spriteRenderer.sortingOrder = Mathf.RoundToInt(-transform.position.y * 100);
+    }
+
+    void Update()
+    {
+        // Atualiza a ordem de renderização em tempo real, se necessário
+        AdjustSortingOrder();
+    }
+}
